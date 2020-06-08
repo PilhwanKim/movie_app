e0ef6c192674842ac4ea0a0ba730f2bd5d9e3be4
Pilhwan Kim <pilhwankim03@gmail.com>
2020-06-09 08:49:42 +0900

    Updates

---
+++ precache-manifest.76a1cd55313a68e2bf6de5317696ec4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b2ae9c31e3e04d46513f0e849f6ba8",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9c95d5fc09a0c6ef4b97",
+    "url": "/movie_app/static/css/main.7bc3fb61.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c95d5fc09a0c6ef4b97",
+    "url": "/movie_app/static/js/main.be8ff4c0.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
